435b1da1aa480d3de9c19579ff3e92d42cb83d6a
Alexis Javier Levano Saire <levanosaire@gmail.com>
2025-08-29 20:41:51 -0500

    fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,14 +1,13 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  verbose: true,
-  preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["ts", "js"],
+  preset: "ts-jest/presets/default-esm",
   extensionsToTreatAsEsm: [".ts"],
-  globals: {
-    "ts-jest": {
-      useESM: true,
-    },
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
   },
+  testEnvironment: "node",
+  verbose: true,
 };
+
+export default config;
