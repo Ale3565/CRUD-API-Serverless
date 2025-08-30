d7cf94a0a22177c7cdbea0bd6ec0beabf3a3c331
Alexis Javier Levano Saire <levanosaire@gmail.com>
2025-08-29 20:03:06 -0500

    update

--- jest.config.ts
+++ jest.config.ts
@@ -1,1 +1,9 @@
-const config: Config = {};
+import type { Config } from "jest";
+
+const config: Config = {
+  verbose: true,
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
+
+export default config;
