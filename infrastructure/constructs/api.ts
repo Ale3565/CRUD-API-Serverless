f375a95abbecb89d318c16b213125ca7d57964b3
Alexis Javier Levano Saire <levanosaire@gmail.com>
2025-08-31 01:03:39 -0500

    construct apigatewayv2 added

--- infrastructure/constructs/api.ts
+++ infrastructure/constructs/api.ts
@@ -1,2 +1,5 @@
 import { Construct } from "constructs";
-import * as api from "aws-cdk-lib/aws-apigatewayv2";
+import * as apigatewayv2 from "aws-cdk-lib/aws-apigatewayv2";
+import * as apigatewayv2Integrations from "aws-cdk-lib/aws-apigatewayv2-integrations";
+
+export class ApiConstruct extends Construct {}
