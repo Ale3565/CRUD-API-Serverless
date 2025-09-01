d38a5fdb4db25f4ef132cc0b40b44c4e5d1a198c
Alexis Javier Levano Saire <levanosaire@gmail.com>
2025-08-31 20:39:45 -0500

    lambda struct updated

--- infrastructure/constructs/lambda.ts
+++ infrastructure/constructs/lambda.ts
@@ -1,11 +1,22 @@
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { DatabaseConstruct } from "./database.js";
+import * as path from "node:path";
 
-interface LambdaContructProps {
+interface LambdaConstructProps {
   table: dynamodb.Table;
 }
 
-export class LambdaContructor extends Construct {
-  constructor() {}
+export class LambdaConstructor extends Construct {
+  public readonly table: dynamodb.Table;
+  constructor(scope: Construct, id: string, props: LambdaConstructProps) {
+    super(scope, id);
+    this.table = props.table;
+    const lambdaFunction = new lambda.Function(this, "UserLambda", {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      handler: "crud-handler.handler",
+      code: lambda.Code.fromAsset(path.join(__filename, "crud-handler")),
+    });
+  }
 }
