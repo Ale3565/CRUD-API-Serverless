d73e1ba7f31ad69e0e156e40d6b5ebe2b2f6414a
Alexis Javier Levano Saire <levanosaire@gmail.com>
2025-09-01 15:56:19 -0500

    struct auth added

---
+++ infrastructure/constructs/auth.ts
@@ -0,0 +1,52 @@
+import * as cognito from "aws-cdk-lib/aws-cognito";
+import { Construct } from "constructs";
+import { Duration } from "aws-cdk-lib";
+
+interface CognitoConstructProps {
+  userPoolName?: string;
+}
+
+export class CognitoConstruct extends Construct {
+  public readonly userPool: cognito.UserPool;
+  public readonly userPoolClient: cognito.UserPoolClient;
+  constructor(scope: Construct, id: string, props: CognitoConstructProps) {
+    super(scope, id);
+    this.userPool = new cognito.UserPool(this, "userPool", {
+      userPoolName: props.userPoolName || "user-crud-pool",
+      signInAliases: {
+        email: true,
+        username: true,
+      },
+      standardAttributes: {
+        email: {
+          required: true,
+          mutable: true,
+        },
+      },
+      passwordPolicy: {
+        minLength: 8,
+        requireDigits: true,
+        requireLowercase: true,
+        requireSymbols: true,
+        requireUppercase: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+    });
+    this.userPoolClient = new cognito.UserPoolClient(this, "UserPoolClient", {
+      userPool: this.userPool,
+      userPoolClientName: "user-crud-client",
+
+      authFlows: {
+        userSrp: true,
+        userPassword: true,
+      },
+
+      generateSecret: false,
+
+      accessTokenValidity: Duration.hours(1),
+      refreshTokenValidity: Duration.days(30),
+    });
+  }
+}
