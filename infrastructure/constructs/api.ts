843bb65dc31e8b88f98b272af87427be0cfc4cbe
Alexis Javier Levano Saire <levanosaire@gmail.com>
2025-08-31 23:14:35 -0500

    lambda construct updated

--- infrastructure/constructs/api.ts
+++ infrastructure/constructs/api.ts
@@ -1,18 +1,27 @@
 import { Construct } from "constructs";
 import * as apigwv2 from "aws-cdk-lib/aws-apigatewayv2";
 import { DatabaseConstruct } from "./database.js";
+import { LambdaConstructor } from "./lambda.js";
+import { HttpLambdaIntegration } from "aws-cdk-lib/aws-apigatewayv2-integrations";
+
 interface ApiConstructProps {
   database: DatabaseConstruct;
+  lambda: LambdaConstructor;
 }
 
 export class ApiConstruct extends Construct {
   public readonly api: apigwv2.HttpApi;
   public readonly url: string;
   public readonly database: DatabaseConstruct;
+  public readonly userIntegration: HttpLambdaIntegration;
 
   constructor(scope: Construct, id: string, props: ApiConstructProps) {
     super(scope, id);
     this.database = props.database;
+    this.userIntegration = new HttpLambdaIntegration(
+      "UserLambdaIntegration",
+      props.lambda.lambdaFunction
+    );
     this.api = new apigwv2.HttpApi(this, "CrudApi", {
       apiName: "user-crud-api",
       corsPreflight: {
@@ -22,5 +31,21 @@ export class ApiConstruct extends Construct {
       },
     });
     this.url = this.api.url || "URL not available";
+
+    this.api.addRoutes({
+      path: "/users",
+      methods: [apigwv2.HttpMethod.GET, apigwv2.HttpMethod.POST],
+      integration: this.userIntegration,
+    });
+
+    this.api.addRoutes({
+      path: "/users/{id}",
+      methods: [
+        apigwv2.HttpMethod.GET,
+        apigwv2.HttpMethod.PUT,
+        apigwv2.HttpMethod.DELETE,
+      ],
+      integration: this.userIntegration,
+    });
   }
 }
--- infrastructure/constructs/lambda.ts
+++ infrastructure/constructs/lambda.ts
@@ -1,7 +1,6 @@
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
-import { DatabaseConstruct } from "./database.js";
 import * as path from "node:path";
 
 interface LambdaConstructProps {
@@ -10,13 +9,15 @@ interface LambdaConstructProps {
 
 export class LambdaConstructor extends Construct {
   public readonly table: dynamodb.Table;
+  public readonly lambdaFunction: lambda.Function;
   constructor(scope: Construct, id: string, props: LambdaConstructProps) {
     super(scope, id);
     this.table = props.table;
-    const lambdaFunction = new lambda.Function(this, "UserLambda", {
+    this.lambdaFunction = new lambda.Function(this, "UserLambda", {
       runtime: lambda.Runtime.NODEJS_22_X,
       handler: "crud-handler.handler",
-      code: lambda.Code.fromAsset(path.join(__filename, "crud-handler")),
+      code: lambda.Code.fromAsset(path.join(__dirname, "../../src/handlers")),
     });
+    this.table.grantReadWriteData(this.lambdaFunction);
   }
 }
